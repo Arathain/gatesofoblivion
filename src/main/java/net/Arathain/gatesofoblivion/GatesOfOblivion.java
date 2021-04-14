fb883946de25c12217de5fbb20cef7fa0cae24f2
Arathain <76701473+Arathain@users.noreply.github.com>
2021-04-14 15:20:17 +0200

    Add Bell Models

---
+++ src/main/java/net/Arathain/gatesofoblivion/GatesOfOblivion.java
@@ -0,0 +1,36 @@
+package net.Arathain.gatesofoblivion;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class GatesOfOblivion implements ModInitializer {
+
+	public static final Item SILVER_BELL = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item RANNA = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item MOSRAEL = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item KIBETH = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item DYRIM = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item BELGAER = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item SARANETH = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item ASTARAEL = new Item( new Item.Settings().group(ItemGroup.TOOLS));
+
+
+	public static void register() {
+		Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "silver_bell"), SILVER_BELL);
+        Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "ranna"), RANNA);
+        Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "mosrael"), MOSRAEL);
+        Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "kibeth"), KIBETH);
+        Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "dyrim"), DYRIM);
+        Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "belgaer"), BELGAER);
+        Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "saraneth"), SARANETH);
+        Registry.register(Registry.ITEM, new Identifier("gatesofoblivion", "astarael"), ASTARAEL);
+	}
+	@Override
+	public void onInitialize() {
+		GatesOfOblivion.register();
+
+	}
+}
